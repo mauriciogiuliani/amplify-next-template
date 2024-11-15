281bec1f410f8893936fdef5cfd2d1fa0ed0ab69
Mauricio Giuliani <mauriciogiuliani@gmail.com>
2024-11-15 13:08:02 -0300

    wip

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,12 +7,12 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  // Todo: a
-  //   .model({
-  //     content: a.string(),
-  //     isDone: a.boolean()
-  //   })
-  //   .authorization((allow) => [allow.publicApiKey()]),
+  Todo: a
+    .model({
+      content: a.string(),
+      isDone: a.boolean()
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 
   Consumidor: a
     .model({
